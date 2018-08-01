50c1395f531b3ef03c7b51c252246c5597281fbb
dghero <devingreyhero@gmail.com>
2018-08-01 13:52:46 -0500

    Complete validation, error handling from server

---
+++ redux-form-app/src/actions/index.js
@@ -0,0 +1,44 @@
+import {SubmissionError} from 'redux-form';
+
+export const onSubmit = values => dispatch => {
+  return fetch('https://us-central1-delivery-form-api.cloudfunctions.net/api/report',{
+      method: 'POST',
+      body: JSON.stringify(values),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res=>{
+      if(!res.ok){
+        if(
+          res.headers.has('content-type') &&
+          res.headers
+            .get('content-type')
+            .startsWith('application/json')
+        ){
+          return res.json().then(err => Promise.reject(err));
+        }
+        return Promise.reject({
+            code: res.status,
+            message: res.statusText
+        })
+      }
+      return
+    })
+    .then(() => console.log('Submitted with values', values))
+    .catch(err =>{
+      console.log('THE ERROR: ', err);
+      const {reason, message, location} = err;
+      if(reason === "ValidationError"){
+        return Promise.reject(
+          new SubmissionError({
+            [location]: message
+          })
+        )
+      }
+      return Promise.reject(
+        new SubmissionError({
+          _error: 'Error submitting message'
+      }))
+    })
+}
--- redux-form-app/src/components/delivery-form.js
+++ redux-form-app/src/components/delivery-form.js
@@ -3,30 +3,68 @@ import './delivery-form.css';
 import {reduxForm, Field} from 'redux-form';
 import Input from './input';
 
+import {onSubmit} from '../actions';
+
+import {required, nonEmpty, exactIdLength, isNumber} from '../validators';
+
 export class DeliveryForm extends Component{
+  
+
   render(){
+    let successMessage;
+    let errorMessage;
+    if(this.props.submitSucceeded){
+      successMessage = (
+        <div className="message message-success">
+          Issue submitted successfully
+        </div>
+      )
+    }
+    if(this.props.error){
+      console.log('error detected');
+      errorMessage = (
+        <div className="message message-error">
+          {this.props.error}
+        </div>
+      )
+    }
+    
     return (
       <div className="form-container">
         <h2>Report a problem with your delivery</h2>
-        <form className="delivery-form">
-          <div className="form-input">
-            <Field id="tracking-number" name="tracking-number" label="Tracking Number" component={Input} element="input" type="text"></Field>
-          </div>
-
-          <div className="form-input">  
-            <Field id="issue-description" name="tracking-number" label="What is your issue?" component={Input} element="select">
-              <option value="not-delivered">My delivery hasn't arrived</option>
-              <option value="wrong-item">The wrong item was delivered</option>
-              <option value="missing-part">Part of my order was missing</option>
-              <option value="damaged">Some of my order arrived damaged</option>
-              <option value="other">Other (give details below)</option>
-            </Field>
-          </div>
-
-          <div className="form-input">
-            <Field id="details" name="details" label="Give more details (optional)" component={Input} element="textarea" type="text"></Field>
-          </div>
-
+        <form className="delivery-form" onSubmit={
+          this.props.handleSubmit(values=>
+            this.props.dispatch(onSubmit(values)))
+        }>
+          {successMessage}
+          {errorMessage}
+          <Field 
+            id="trackingNumber"
+            name="trackingNumber" 
+            label="Tracking Number" 
+            component={Input} 
+            element="input"
+            type="text"
+            validate={[required, nonEmpty, exactIdLength, isNumber]}
+            ></Field>
+        
+          <Field id="issue"
+            name="issue"
+            label="What is your issue?"
+            component={Input}
+            element="select"
+            validate={[required]}
+            >
+            <option>Select issue</option>
+            <option value="not-delivered">My delivery hasn't arrived</option>
+            <option value="wrong-item">The wrong item was delivered</option>
+            <option value="missing-part">Part of my order was missing</option>
+            <option value="damaged">Some of my order arrived damaged</option>
+            <option value="other">Other (give details below)</option>
+          </Field>
+        
+          <Field id="details" name="details" label="Give more details (optional)" component={Input} element="textarea" type="text"></Field>
+          
           <button>Submit</button>
         </form>
       </div>
